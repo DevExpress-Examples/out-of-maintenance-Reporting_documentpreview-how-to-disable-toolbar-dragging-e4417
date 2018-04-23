2182252c69dd9b49f75bacad23bb189fcbedf69d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DocumentPreview - how to disable toolbar dragging
+
+
+<p>To prevent Bar from being dragged by a user, set the <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfBarsBar_ShowDragWidgettopic">Bar.ShowDragWidget Property </a> of the "DocumentPreviewToolBar" to false in the <strong>BarManager.Loaded</strong> event handler instead of the corresponding event handler of the main window.</p>
+
+<br/>
+
+
---
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.Printing
+' ...
+
+Namespace MinimalisticReportPreviewDemo
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+
+			Dim report As New Report()
+
+			Dim model As New XtraReportPreviewModel(report)
+			report.CreateDocument()
+			preview.Model = model
+		End Sub
+
+		Private Sub barManager_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			barManager.Bars("DocumentPreviewToolBar").ShowDragWidget = False
+		End Sub
+	End Class
+End Namespace
