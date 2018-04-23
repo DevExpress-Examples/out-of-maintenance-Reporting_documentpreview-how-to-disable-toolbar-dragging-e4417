6e76639fd070e0fd6b659d2fd2daab62d95a78f8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MinimalisticReportPreviewDemo/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Printing;
+// ...
+
+namespace MinimalisticReportPreviewDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            Report report = new Report();
+
+            XtraReportPreviewModel model = new XtraReportPreviewModel(report);
+            report.CreateDocument();
+            preview.Model = model;
+        }
+
+        private void barManager_Loaded(object sender, RoutedEventArgs e) {
+            barManager.Bars["DocumentPreviewToolBar"].ShowDragWidget = false;
+        }
+    }
+}
