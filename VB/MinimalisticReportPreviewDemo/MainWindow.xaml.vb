fb948016b2690b0587b37982f93ef9b77166e978
DevExpressExampleBot <fakeEmail>
2021-09-02 19:41:12 +0300

    Source auto update [skip ci]

--- VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -2,22 +2,22 @@
 ' ...
 
 Namespace MinimalisticReportPreviewDemo
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
 
-            Dim report As New Report()
-            preview.DocumentSource = report
-            report.CreateDocument()
-        End Sub
+			Dim report As New Report()
+			preview.DocumentSource = report
+			report.CreateDocument()
+		End Sub
 
-        Private Sub barManager_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            barManager.Bars("DocumentPreviewToolBar").ShowDragWidget = False
-        End Sub
-    End Class
+		Private Sub barManager_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			barManager.Bars("DocumentPreviewToolBar").ShowDragWidget = False
+		End Sub
+	End Class
 End Namespace
